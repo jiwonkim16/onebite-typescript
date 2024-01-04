f9b69115ac4d54b977dc35ced633176580f052c6
Kim Jiwon <stayby16@kakao.com>
2024-01-04 20:15:40 +0900

    🚀 object type

---
+++ section2/src/chapter3.ts
@@ -0,0 +1,16 @@
+// object
+let user: {
+  id?: number;
+  name: string;
+} = {
+  id: 1,
+  name: "jiwon",
+};
+
+let config: {
+  readonly apiKey: string;
+} = {
+  apiKey: "My api key",
+};
+
+// config.apiKey = "hello";
